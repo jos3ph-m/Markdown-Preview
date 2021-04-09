498d73bed56b4146008d72325b070a78c1e4e643
Joseph M <jmedina1985@hotmail.com>
2021-04-08 21:38:17 -0700

    Basic structure

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           value={markdown}
           onChange={(e) => setMarkdown(e.target.value)}
         ></textarea>
+        <article className="result">{markdown}</article>
       </section>
     </main>
   );
